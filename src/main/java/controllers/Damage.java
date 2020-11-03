ce8183d1f816bcd037b1d72b8ab8efedaf732990
92324 <92324@farnborough.ac.uk>
2020-11-03 13:29:07 +0000

    argh

---
+++ src/main/java/controllers/Damage.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import server.Main;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Path("Damage/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class Damage{
+    @GET
+    @Path("list")
+    public String DamageList() {
+        System.out.println("Invoked Ammo.AmmoList()");
+        JSONArray response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT DamageID, Damage, Penetration, Caliber, AmmoID, FROM Damagelists");
+            ResultSet results = ps.executeQuery();
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("DamageID", results.getInt(1));
+                row.put("Damage", results.getString(2));
+                row.put("Penetration", results.getString(3));
+                row.put("Caliber", results.getString(4));
+                row.put("AmmoID", results.getString(5));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+}
