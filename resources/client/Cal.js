d12bed332e69823fc671f5b81b56b6416f566401
Henry-Grace <92324@farnborough.ac.uk>
2021-01-18 10:52:30 +0000

    ACTUALLY WORKS

--- resources/client/Cal.js
+++ resources/client/Cal.js
@@ -2,10 +2,13 @@
 
 function MyFunction() {
     const elem = document.getElementById('head');
-    let val1 = elem
-    if (val1.value==="Head 35/35") {
+    let val1 = elem.outerHTML
+    if (elem.value !=="Head 0/35") {
+        let num1 = val1.substring(53, 55);
+        let num2 = parseInt(num1);
+        num2 = num2 - 1
+        let word2 = num2
         let word1 = "Head "
-        let word2 = "34"
         let word3 = "/35"
         let cent = word1 + word2 +word3;
         elem.value = cent;
