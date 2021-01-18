94338751bf0c611fdab8960d90dc4b0984e9e527
Henry-Grace <92324@farnborough.ac.uk>
2021-01-18 10:28:29 +0000

    ACTUALLY WORKS

--- resources/client/Cal.js
+++ resources/client/Cal.js
@@ -1,9 +1,15 @@
+
+
 function MyFunction() {
     const elem = document.getElementById('head');
-
-    if (elem.value==="Head 35/35") {
-        elem.value = "Head 34/35";
-        elem.innerHTML = "Head 34/35";
+    let val1 = elem
+    if (val1.value==="Head 35/35") {
+        let word1 = "Head "
+        let word2 = "34"
+        let word3 = "/35"
+        let cent = word1 + word2 +word3;
+        elem.value = cent;
+        elem.innerHTML = cent;
     }
     else {
         elem.value = "Shitty Test";
